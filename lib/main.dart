c0ee7b693efa7f1dd0f6fa7c868fe83b5c2c11cf
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2023-02-18 22:56:40 +0530

    CustomPaint

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,34 +11,69 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-        child: Center(
-          child: CupertinoButton(
-            child: Text("click me"),
-            onPressed: (){
-              showCupertinoModalPopup(context: context, builder: (BuildContext builder){
-                return CupertinoPopupSurface(
-                    child: Container(
-                  color: CupertinoColors.white,
-                  alignment: Alignment.center,
-    width: double.infinity,
-    height: 400,
-    child: Center(
-    child: CupertinoButton(
-    child: Text('Close'),
-    onPressed: (){
-      Navigator.of(context).pop();
-    },
-    ),
-    ),
-                ),
-                );
-              },
-                );
-            },
-          ),
-        ),
+    return MaterialApp(
+      // showSemanticsDebugger: true,
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: const MyHomePage(title: 'Flutter Widegts'),
     );
   }
 }
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    bool isSelected = false;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Center(
+            child: CustomPaint(
+              child: Text(
+                'This is Pacman',
+                style: TextStyle(
+                  color: Colors.black,
+                  backgroundColor: Colors.white54,
+                  fontSize: 30,
+                ),
+              ),
+              painter: DemoPainter(),
+            )
+        ),
+      )
+    );
+  }
+}
+
+class DemoPainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size){
+    var center = size/2;
+    var paint = Paint()..color = Colors.yellow;
+    canvas.drawArc(
+        Rect.fromCenter(center: Offset(center.width,center.height), width: 250, height: 250,),
+        .4, 2*3.14-.8, true, paint);
+  }
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate){
+    return false;
+  }
+}
+
 
