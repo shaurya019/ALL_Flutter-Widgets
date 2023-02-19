4f5dd8fcb3c71bb58e523e0cd26458303f523878
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2023-02-19 22:49:59 +0530

    FadeInImage

--- lib/main.dart
+++ lib/main.dart
@@ -33,45 +33,18 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-
   @override
   Widget build(BuildContext context) {
-   bool _isExpanded = false;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-     body: Column(
-       crossAxisAlignment: CrossAxisAlignment.start,
-       children: <Widget>[
-         Container(
-           color: Colors.orangeAccent,
-           child: Row(
-             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-               children: <Widget>[
-                 Container(
-                   padding: EdgeInsets.all(10.0),
-                   child: Text('Title',style: TextStyle(color: Colors.white,fontSize: 22),),
-                 ),
-                 ExpandIcon(
-                   isExpanded: _isExpanded,
-                   color: Colors.white,
-                   expandedColor: Colors.black,
-                   onPressed: (bool isExpanded){
-                    setState(() {
-                      _isExpanded = !isExpanded;
-                    });
-                   },
-                 )
-               ]
-           ),
-         ),
-         if(_isExpanded)
-           Padding(
-             padding:EdgeInsets.all(15.0),
-             child: Text('Hi'),
-           ),
-       ],
+     body:Center(
+       child: FadeInImage.assetNetwork(
+           placeholder: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg',
+           image: 'https://cdn.pixabay.com/photo/2016/10/21/14/50/plouzane-1758197_1280.jpg',
+       ),
      ),
     );
   }
