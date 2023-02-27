10d21f038f67fb2d711de4ccb0b02ad8c9c75425
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2023-02-27 23:53:57 +0530

    BoxShadow

--- lib/main.dart
+++ lib/main.dart
@@ -18,37 +18,56 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: const MyHomePage(title: 'Flutter Widegts'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin{
-  int _counter = 0;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body:Center(
-        child: Image.network(
-          'https://tinyurl.com/yc4pctt5',
-          color: Colors.blue,
-          colorBlendMode: BlendMode.colorBurn,
-        ),
+  Widget build(BuildContext context){
+    return Center(
+      child: Icon(
+          Icons.flutter_dash,
+         color: Colors.orangeAccent,
+        size: 200,
+        shadows: [
+          BoxShadow(
+            color: Colors.black,
+            offset: Offset(8,8),
+            spreadRadius: 10,
+            blurRadius: 10,
+          ),
+        ],
       ),
     );
   }
+  // const MyHomePage({super.key, required this.title});
+  // final String title;
+  //
+  // @override
+  // State<MyHomePage> createState() => _MyHomePageState();
 }
+
+// class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin{
+//   int _counter = 0;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body:Center(
+//         child: Image.network(
+//           'https://tinyurl.com/yc4pctt5',
+//           color: Colors.blue,
+//           colorBlendMode: BlendMode.colorBurn,
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 
 
