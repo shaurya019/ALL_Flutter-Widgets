9d92aade895810d8b836a7c9f962f6187a952a20
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2023-03-01 01:40:29 +0530

    RefreshIndicator Widget

--- lib/main.dart
+++ lib/main.dart
@@ -35,28 +35,33 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin{
-  final ScrollController controller = ScrollController();
+  List<String> items = [
+    "Item 1",
+    "Item 2",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body:Center(
-        child: RichText(
-          text: TextSpan(
-            style: TextStyle(color: Colors.orangeAccent,fontSize: 30),
-            children: <TextSpan>[
-              TextSpan(text: 'To the  '),
-              TextSpan(
-                text: 'moon  ',
-                style: TextStyle(color: Colors.white,),
-              ),
-              TextSpan(text: 'and beyond'),
-            ],
-          ),
-        )
-      ),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          await Future.delayed(Duration(seconds: 1),);
+          int nextItem = items.length+1;
+          items.add("items $nextItem");
+          setState(() {});
+          },
+        child: ListView.builder(
+            itemCount: items.length,
+            itemBuilder: ((context,index) => Padding(padding: EdgeInsets.all(8.0),
+            child: ListTile(
+              title: Text(items[index]),
+              tileColor: Colors.white24,
+            ),)),
+        ),
+        color: Colors.white,
+      )
     );
   }
 }
