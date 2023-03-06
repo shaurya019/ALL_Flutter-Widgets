2ad784581fd21e320fd8d7666c7c1b02cad2449a
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2023-03-06 16:36:51 +0530

    ScaleTransition

--- lib/main.dart
+++ lib/main.dart
@@ -32,61 +32,40 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin{
+  late final AnimationController _controller = AnimationController(
+    duration: Duration(seconds: 1),
+    vsync: this,
+  )..repeat(reverse: true);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  late final Animation<double> _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.fastOutSlowIn,
+  );
+  @override
+
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
-  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: _callNumber,
-          child: Text('Call Number'),
+        child:ScaleTransition(
+          scale: _animation,
+          child: FlutterLogo(size: 150.0,),
         ),
       ),
     );
   }
 }
 
-_callNumber() async{
-  const number = '8383867619'; //set the number here
-  bool? res = await FlutterPhoneDirectCaller.callNumber(number);
-}
