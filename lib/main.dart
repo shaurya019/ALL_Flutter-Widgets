ffaa2603d6fe8dacff51c8202799eefa2a03a843
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2023-03-06 16:52:22 +0530

    ClipPath

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,21 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -38,37 +27,42 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin{
-  late final AnimationController _controller = AnimationController(
-    duration: Duration(seconds: 1),
-    vsync: this,
-  )..repeat(reverse: true);
-
-  late final Animation<double> _animation = CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeInCirc,
-  );
   @override
+  Widget build(BuildContext context) {
+    return  ClipPath(
+        clipper: MyClipper(),
+        child: Container(
+          width: double.infinity,
+          height: 300,
+          color: Color(0xFF142B46),
+        ),
+      );
+  }
+}
 
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+class MyClipper extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size){
+    return Path()
+      ..lineTo(0,size.height)
+        ..quadraticBezierTo(
+            size.width/4,
+            size.height-40,
+            size.width/2,
+            size.height-20,
+        )
+      ..quadraticBezierTo(
+        size.width * 3/4,
+        size.height,
+        size.width,
+        size.height-30,
+      )
+          ..lineTo(size.width,0);
   }
 
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child:ScaleTransition(
-          turns: _animation,
-          child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: FlutterLogo(size: 150.0,),
-          ),
-        ),
-      ),
-    );
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper){
+    return false;
   }
 }
 
