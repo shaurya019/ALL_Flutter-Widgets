f26abd6ea6896166e7b965365d6d943038278e92
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2023-03-08 00:48:00 +0530

    Text-feild

--- lib/main.dart
+++ lib/main.dart
@@ -28,44 +28,80 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin{
- var arrData = [
-   {
-     'name':'Tanuja',
-     'mno':'9717054182',
-     'age':'43',
-   },
-   {
-     'name':'Jitander',
-     'mno':'9899963630',
-     'age':'51',
-   },
-   {
-     'name':'Shaurya',
-     'mno':'8383867619',
-     'age':'22',
-   }
- ];
-  @override
+ var emailText = TextEditingController();
+ var passText = TextEditingController();
+   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Widget',),
       ),
-      body: Container(
-        child: ListView(
-          children: arrData.map((value) =>
-            ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text(value['name'].toString()),
-              subtitle: Text(value['mno'].toString()),
-              trailing: CircleAvatar(
-                radius:15,
-                backgroundColor: Colors.blueAccent,
-                child:Text(value['age'].toString()),)
-            ),
-          ).toList(),
+      body: Center(
+        child: Container(
+          width: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+            controller: emailText,
+          decoration: InputDecoration(
+            hintText: 'Enter Email',
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(11),
+            borderSide: BorderSide(
+              color: Colors.deepOrange,
+              width: 2,
+            )
         ),
+            enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(11),
+        borderSide: BorderSide(
+          color: Colors.blueAccent,
+          width: 2,
+        )
+        ),
+
+            prefixIcon: Icon(Icons.email,color: Colors.orange,),
+        ),
+        ),
+              SizedBox(height: 30,),
+              TextField(
+      controller: passText,
+    obscureText: true,
+    decoration: InputDecoration(
+      hintText: 'Enter password',
+    focusedBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(11),
+    borderSide: BorderSide(
+    color: Colors.deepOrange,
+    width: 2,
+    )
+    ),
+    enabledBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(11),
+    borderSide: BorderSide(
+    color: Colors.blueAccent,
+    width: 2,
+    )
+    ),
+      suffixIcon: IconButton(
+        icon: Icon(Icons.remove_red_eye,color: Colors.orange,),
+        onPressed: (){
+
+        },
       ),
+      prefixIcon: Icon(Icons.password,color: Colors.orange,),
+    ),
+    ),
+              ElevatedButton(onPressed: (){
+                String uEmail = emailText.text.toString();
+                String uPass = passText.text.toString();
+print("Email: $uEmail,Pass:$uPass");
+              }, child: Text('Login'))
+            ],
+          ),
+        ),
+      )
     );
   }
 }
