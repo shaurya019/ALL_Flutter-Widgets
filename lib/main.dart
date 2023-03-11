a145d795a8f39974018b8c4178ade884a837287a
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2023-03-11 21:43:39 +0530

    AnimatedAlign

--- lib/main.dart
+++ lib/main.dart
@@ -28,80 +28,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin{
- var emailText = TextEditingController();
- var passText = TextEditingController();
+bool f = false;
    @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Widget',),
-      ),
-      body: Center(
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          f = !f;
+        });
+      },
+      child: Center(
         child: Container(
-          width: 300,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-            controller: emailText,
-          decoration: InputDecoration(
-            hintText: 'Enter Email',
-          focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(11),
-            borderSide: BorderSide(
-              color: Colors.deepOrange,
-              width: 2,
-            )
-        ),
-            enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(11),
-        borderSide: BorderSide(
-          color: Colors.blueAccent,
-          width: 2,
-        )
-        ),
-
-            prefixIcon: Icon(Icons.email,color: Colors.orange,),
-        ),
-        ),
-              SizedBox(height: 30,),
-              TextField(
-      controller: passText,
-    obscureText: true,
-    decoration: InputDecoration(
-      hintText: 'Enter password',
-    focusedBorder: OutlineInputBorder(
-    borderRadius: BorderRadius.circular(11),
-    borderSide: BorderSide(
-    color: Colors.deepOrange,
-    width: 2,
-    )
-    ),
-    enabledBorder: OutlineInputBorder(
-    borderRadius: BorderRadius.circular(11),
-    borderSide: BorderSide(
-    color: Colors.blueAccent,
-    width: 2,
-    )
-    ),
-      suffixIcon: IconButton(
-        icon: Icon(Icons.remove_red_eye,color: Colors.orange,),
-        onPressed: (){
-
-        },
-      ),
-      prefixIcon: Icon(Icons.password,color: Colors.orange,),
-    ),
-    ),
-              ElevatedButton(onPressed: (){
-                String uEmail = emailText.text.toString();
-                String uPass = passText.text.toString();
-print("Email: $uEmail,Pass:$uPass");
-              }, child: Text('Login'))
-            ],
+          width: double.infinity,
+          height: 250.0,
+          color: Colors.grey,
+          child: AnimatedAlign(
+            alignment: f ? Alignment.topRight : Alignment.bottomLeft,
+            duration: Duration(seconds: 1),
+            curve: Curves.fastOutSlowIn,
           ),
         ),
-      )
+      ),
     );
   }
 }
